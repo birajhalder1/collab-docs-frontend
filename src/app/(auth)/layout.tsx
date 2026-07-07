8ba933c3ad81bd3562fffd7c6cac52655b945ac2
crew19 <nstdev22@gmail.com>
2026-07-07 16:13:03 +0530

    Add Frontend CI

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,9 +4,16 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-100 p-6">
-      <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-xl">
-        {/* <div className="mb-8 text-center">
+    <aside
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/image/bgImage.jpg')" }}
+    >
+      <main
+        className="flex min-h-screen items-center justify-center bg-slate-100 p-6"
+        style={{ backgroundColor: "rgba(255,255,255,0.2)" }}
+      >
+        <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-xl">
+          {/* <div className="mb-8 text-center">
           <h1 className="text-3xl font-bold text-slate-900">
             Collab Editor
           </h1>
@@ -16,8 +23,9 @@ export default function AuthLayout({
           </p>
         </div> */}
 
-        {children}
-      </div>
-    </main>
+          {children}
+        </div>
+      </main>
+    </aside>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,34 @@ import Button from "@/components/ui/Button";
 import { useRouter } from "next/navigation";
 
 export default function HomePage() {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-100">
-      <div className="w-full max-w-lg rounded-2xl bg-white p-10 shadow-xl">
-        <h1 className="text-center text-4xl font-bold">
-          Local-First Collaborative Editor
-        </h1>
+    <aside
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/image/bgImage.jpg')" }}
+    >
+      <main className="flex min-h-screen items-center justify-center bg-slate-100" style={{ backgroundColor: "rgba(255,255,255,0.2)" }}>
+        <div className="w-full max-w-lg rounded-2xl bg-white p-10 shadow-xl">
+          <h1 className="text-center text-4xl font-bold">
+            Local-First Collaborative Editor
+          </h1>
 
-        <p className="mt-4 text-center text-gray-500">
-          Offline First • Real-time Collaboration • Version History
-        </p>
+          <p className="mt-4 text-center text-gray-500">
+            Offline First • Real-time Collaboration • Version History
+          </p>
 
-        <div className="mt-8 flex justify-center gap-4">
-          <Button onClick={()=> router.push("/login")}>Login</Button>
+          <div className="mt-8 flex justify-center gap-4">
+            <Button onClick={() => router.push("/login")}>Login</Button>
 
-          <Button variant="secondary" onClick={()=> router.push("/register")}>
-            Register
-          </Button>
+            <Button
+              variant="secondary"
+              onClick={() => router.push("/register")}
+            >
+              Register
+            </Button>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </aside>
   );
 }
